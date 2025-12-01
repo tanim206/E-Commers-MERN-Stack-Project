9ce29be5928af965160e5b024df4c0caf348dd6a
tanim246 <mdtasnimhossaintanim@gmail.com>
2025-12-01 11:42:08 +0600

    update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-
 const PORT = 3001;
 
-app.listen(MessagePort, ()=>{
+app.use(morgan('dev'))
+app.get("/", (req, res) => {
+    res.status(200).send({
+        statusCode: "200",
+        message: "Hello World"
+    });
+});
+
+app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
 });
