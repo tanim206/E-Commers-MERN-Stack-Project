9e58e61cdbefd2054e6387d7f1c8c5b372a04d21
tanim246 <mdtasnimhossaintanim@gmail.com>
2025-12-16 14:50:38 +0600

    code adds

--- src/server.js
+++ src/server.js
@@ -2,12 +2,33 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const PORT = 3001;
-
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const isLoggedIn = (req, res, next) => {
+    const login = true;
+    if (login) {
+        req.body.id = 101;
+        next();
+    } else {
+        return res.status(401).json({
+            message: "please login first"
+        });
+    };
+};
+
 app.get("/", (req, res) => {
     res.status(200).send({
         statusCode: "200",
         message: "Hello World"
+    });
+});
+app.get("/api/user", isLoggedIn, (req, res) => {
+    console.log(req.body.id);
+    res.status(200).send({
+        statusCode: "200",
+        message: "User profile create"
     });
 });
 
