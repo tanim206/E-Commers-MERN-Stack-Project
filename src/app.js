bb17489f8d765f90b9c8586542cdf24c326e10dd
tanim206 <mdtanim.dev@gmail.com>
2025-12-19 21:47:53 -0800

    init my codes

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const createErrors = require("http-errors");
+const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const app = express();
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minite
+  max: 5,
+  message: "Too Many Request From This IP. Please Try Again",
+});
+
+app.use(xssClean());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/test", rateLimiter, (req, res) => {
+  res.status(200).send({
+    statusCode: "200",
+    message: "Hello World",
+  });
+});
+app.get("/api/users", (req, res) => {
+  console.log(req.body.id);
+  res.status(200).send({
+    statusCode: "200",
+    message: "User profile create",
+  });
+});
+// client Error
+app.use((req, res, next) => {
+  next(createErrors(404, "route not found"));
+});
+// server Error ----- Sob error aikane asbe
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
+});
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,47 +1,5 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const app = express();
+const app = require("./app");
 const PORT = 3001;
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const isLoggedIn = (req, res, next) => {
-  const login = true;
-  if (login) {
-    req.body.id = 101;
-    next();
-  } else {
-    return res.status(401).json({
-      message: "please Login first",
-    });
-  }
-};
-
-app.get("/", (req, res) => {
-  res.status(200).send({
-    statusCode: "200",
-    message: "Hello World",
-  });
-});
-app.get("/api/users", isLoggedIn, (req, res) => {
-  console.log(req.body.id);
-  res.status(200).send({
-    statusCode: "200",
-    message: "User profile create",
-  });
-});
-// client Error
-app.use((req, res, next) => {
-  res.status(404).json({ message: "route not found" });
-  next();
-});
-// server Error
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Something Broke" });
-});
 
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
