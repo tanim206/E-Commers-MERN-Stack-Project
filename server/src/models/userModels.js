3b9b6fb59dd68d6dc6750ba29fc4a497c82d3c22
tanim206 <mdtanim.dev@gmail.com>
2025-12-22 04:56:26 -0800

    User Schema add

--- server/src/models/userModels.js
+++ server/src/models/userModels.js
@@ -1,18 +1,58 @@
-const { Schema } = require("mongoose");
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const { defaultImagePath } = require("../secret");
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+      minlength: [3, "User name must be at least 3 characters"],
+      maxlength: [30, "User name can be maximum 30 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: (v) => {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(v);
+        },
+      },
+      message: "enter a valid email",
+    },
+    password: {
+      type: String,
+      required: [true, "User password is required"],
+      minlength: [6, "User name must be at least 6 characters"],
+      set: (v) => bcrypt.hashSync(v, bcrypt.genSaltSync(10)),
+    },
+    image: {
+      type: String,
+      default: defaultImagePath,
+    },
+    address: {
+      type: String,
+      require: [true, "User address is required"],
+    },
+    phone: {
+      type: String,
+      require: [true, "User phone is required"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isBanned: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "User name is required"],
-    trim: true,
-    minlength: [2, "User name must be at least 2 characters"],
-    maxlength: [30, "User name can be maximum 30 characters"],
-  },
-  name: {
-    type: String,
-    required: [true, "User name is required"],
-    trim: true,
-    minlength: [2, "User name must be at least 2 characters"],
-    maxlength: [30, "User name can be maximum 30 characters"],
-  },
-});
+const User = model("User", userSchema);
+
+module.exports = User;
--- server/src/secret.js
+++ server/src/secret.js
@@ -3,4 +3,7 @@ const serverPort = process.env.SERVER_PORT || 3002;
 const mongodbURL =
   process.env.MONGODB_ATLAS_URL || "mongodb://localhost:27017/ecommarsDB";
 
-module.exports = { serverPort, mongodbURL };
+const defaultImagePath = process.env.DEFAULT_USER_IMAGE_PATH || "server/public/image/users/default.png"
+
+
+module.exports = { serverPort, mongodbURL , defaultImagePath};
